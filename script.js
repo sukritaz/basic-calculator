09c91386a0285e1764adc2eccc9b6aa9a5f95278
sukritaz <sukri.pani@gmail.com>
2023-12-26 17:47:14 +0530

    Creating buttons with a more organised approach and hooks.

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
 const calculator = document.getElementById("calculator");
-calculator.style.width = `${innerWidth/3}px`;
-calculator.style.height = `${innerWidth/3}px`;
+calculator.style.width = `${innerWidth / 4}px`;
+calculator.style.height = `${innerWidth / 3}px`;
 
 createNumbers();
 
-function appendButtonToRow(row, buttonText, buttonId, buttonClass) {
-    const button = document.createElement('button');
-    button.textContent = buttonText;
-    button.id = buttonId;
-    button.className = buttonClass;
-    row.appendChild(button);
-}
-function createNumbers () {
+
+function createNumbers() {
     const rows = document.querySelectorAll('#buttons .row');
     rows.forEach((row, index) => {
-        switch(index) {
+        switch (index) {
             case 0:
-                appendButtonToRow(row, 'ADD', 'add', 'operation');
-                appendButtonToRow(row, 'SUB', 'sub', 'operation');
-                appendButtonToRow(row, 'MUL', 'mul', 'operation');
-                appendButtonToRow(row, 'DIV', 'div', 'operation');
+                appendButtonToRow(row, 'ADD', 'add', 'operation', handleOperationButtonClick);
+                appendButtonToRow(row, 'SUB', 'sub', 'operation', handleOperationButtonClick);
+                appendButtonToRow(row, 'MUL', 'mul', 'operation', handleOperationButtonClick);
+                appendButtonToRow(row, 'DIV', 'div', 'operation', handleOperationButtonClick);
                 break;
             case 1:
-                appendButtonToRow(row, '1', 'num1', 'number');
-                appendButtonToRow(row, '2', 'num2', 'number');
-                appendButtonToRow(row, '3', 'num3', 'number');
-                appendButtonToRow(row, 'DEL', 'del', 'operation');
+                appendButtonToRow(row, '1', 'num1', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '2', 'num2', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '3', 'num3', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, 'DEL', 'del', 'operation', handleOperationButtonClick);
                 break;
             case 2:
-                appendButtonToRow(row, '4', 'num4', 'number');
-                appendButtonToRow(row, '5', 'num5', 'number');
-                appendButtonToRow(row, '6', 'num6', 'number');
-                appendButtonToRow(row, '.', 'decimal', 'operation');
+                appendButtonToRow(row, '4', 'num4', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '5', 'num5', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '6', 'num6', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '.', 'decimal', 'operation', handleOperationButtonClick);
                 break;
             case 3:
-                appendButtonToRow(row, '7', 'num7', 'number');
-                appendButtonToRow(row, '8', 'num8', 'number');
-                appendButtonToRow(row, '9', 'num9', 'number');
-                appendButtonToRow(row, '0', 'num0', 'number');
+                appendButtonToRow(row, '7', 'num7', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '8', 'num8', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '9', 'num9', 'number', handleNumberButtonClick);
+                appendButtonToRow(row, '0', 'num0', 'number', handleNumberButtonClick);
                 break;
 
         }
     })
+}
+
+function appendButtonToRow(row, buttonText, buttonId, buttonClass, clickHandler) {
+    const button = document.createElement('button');
+    button.textContent = buttonText;
+    button.id = buttonId;
+    button.className = buttonClass;
+    button.addEventListener('click', clickHandler);
+    row.appendChild(button);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a, b ){
+    return b == 0 ? NaN : a/b;
+}
+
+function handleNumberButtonClick() {
+    
+}
+
+function handleOperationButtonClick() {
+    
 }
